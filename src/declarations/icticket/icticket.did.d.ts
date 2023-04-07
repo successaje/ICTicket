7edadad128690811b3e25dae60dc8b2fe3daf7b3
Mohd Rejoan <wearecrx@gmail.com>
2023-04-07 12:34:38 +0600

    all api connected with front-end

--- src/declarations/icticket/icticket.did.d.ts
+++ src/declarations/icticket/icticket.did.d.ts
@@ -3,9 +3,16 @@ import type { ActorMethod } from '@dfinity/agent';
 
 export type Result = { 'ok' : bigint } |
   { 'err' : string };
+export interface Ticket {
+  'owner' : Principal,
+  'statement' : string,
+  'generatedAt' : bigint,
+}
 export interface TicketGenerator {
   'generate' : ActorMethod<[Principal], Result>,
+  'getOwner' : ActorMethod<[bigint], [] | [Ticket]>,
   'greet' : ActorMethod<[string], string>,
   'multipleGenerate' : ActorMethod<[Principal, bigint], Array<bigint>>,
+  'whoami' : ActorMethod<[], Principal>,
 }
 export interface _SERVICE extends TicketGenerator {}
